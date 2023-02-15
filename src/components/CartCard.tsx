4b3c4ccb6c11a0b7ea971257045ff03a063d75f6
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-02-15 17:25:00 +0300

    made the cart more responsive

--- src/components/CartCard.tsx
+++ src/components/CartCard.tsx
@@ -17,10 +17,10 @@ export default function CartCard(props: CartCardProps) {
         <div className="w-full flex flex-row border-[1px] rounded-xl mb-5 shadow-md h-[150px] xl:h-56 ">
             <div style={{ backgroundImage: `url(${props.img})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-[35%] bg-slate-400 rounded-l-xl"></div>
             <div className="w-[65%] h-full  flex flex-col">
-                <div className=" flex lg:pt-16 pl-3"><h2 className="text-2xl mr-1">{props.title}</h2><p className="text-gray-600 text-md mt-2">x{props.countInCart}</p></div>
+                <div className=" flex lg:pt-16 pl-3"><h2 className="text-sm lg:text-2xl mr-1 mt-1">{props.title}</h2><p className="text-gray-600 text-xs lg:text-md mt-2">x{props.countInCart}</p></div>
 
                 <p className="text-sm lg:text-lg text-gray-500 pl-3 pt-2">${props.price}</p>
-                <div className="flex ml-auto pr-5 mt-[-2.25rem]"> <p className=" text-xl lg:text-2xl mr-[-25px] mt-[-10px]  lg:mr-0">${props.price * props.countInCart}</p> <button onClick={removeItemFromCart} className=" mt-8 lg:mt-0 bg-white text-red-500 border-[1px] hover:bg-red-100 border-red-500 ml-2 w-9 h-9 rounded">X</button></div>
+                <div className="flex ml-auto pr-5 mt-[-2.25rem]"> <p className="t text-md lg:text-2xl mr-[-25px] mt-[-10px]  lg:mr-0">${props.price * props.countInCart}</p> <button onClick={removeItemFromCart} className=" mt-8 lg:mt-0 bg-white text-red-500 border-[1px] hover:bg-red-100 border-red-500 ml-2 w-9 h-9 rounded">X</button></div>
             </div>
 
         </div>
